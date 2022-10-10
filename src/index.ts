5e76369280b9599fdaa644edaf87103216ab9761
Jesse Conner <jconner90@gmail.com>
2022-10-10 16:48:45 -0400

    Fix index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-import Crawler from "./Crawler";
-import path from "path";
-
-const entryPointRaw = process.argv[2];
-const crawler = new Crawler(entryPointRaw, path.resolve('.', 'captures'), 1, console.log);
-await crawler.crawl();
-export {};
+import c from "./Crawler";
+export default c;
