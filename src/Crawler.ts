28a2c3cabd79bff5b94a3c4cf2eaa16c5240df34
Jesse Conner <jconner90@gmail.com>
2023-01-07 07:59:04 -0500

    Taking new options

--- src/Crawler.ts
+++ src/Crawler.ts
@@ -1,4 +1,4 @@
-import type {Page, PageList} from '@types';
+import type {ConcreteSnapshotOptions, Page, PageList, SnapshotOptions} from '@types';
 import Scraper from './Scraper.js';
 import Path from 'path';
 import {bundle, createDir, sanitizeFileName} from './helpers/files.js';
@@ -7,57 +7,73 @@ import {log, setLogFunction, setLogLevel} from './logger.js';
 import fs from 'fs';
 
 export default class Crawler {
-	private readonly pageList: PageList;
-	private readonly entryPoint: URL;
-	private readonly captureDir: string;
+    private readonly pageList: PageList;
+    private readonly entryPoint: URL;
+    private readonly captureDir: string;
 
-	public constructor(rawEntryPoint: string, captureDir: string, logLevel: 1 | 2 | 3 = 1, onEvent: (msg: string) => unknown = console.log) {
-		this.pageList = new Map<string, Page>();
-		this.entryPoint = new URL(rawEntryPoint);
-		this.pageList.set(rawEntryPoint, this.createSeed());
-		this.captureDir = Path.join(captureDir, sanitizeFileName(this.entryPoint.host), sanitizeFileName(new Date().toLocaleString()));
-		setLogLevel(logLevel);
-		setLogFunction(onEvent);
-	}
+    private readonly options: ConcreteSnapshotOptions;
 
-	public async crawl() {
-		const {entryPoint, pageList, captureDir} = this;
+    public constructor(rawEntryPoint: string, captureDir: string, options: SnapshotOptions = {}) {
+        this.options = this.mergeOptions(options);
+        this.pageList = new Map<string, Page>();
+        this.entryPoint = new URL(rawEntryPoint);
+        this.pageList.set(rawEntryPoint, this.createSeed());
+        this.captureDir = Path.join(captureDir, sanitizeFileName(this.entryPoint.host), sanitizeFileName(new Date().toLocaleString()));
+        const {logLevel, onEvent} = this.options;
+        setLogLevel(logLevel);
+        setLogFunction(onEvent);
+    }
 
-		const scraper = new Scraper(pageList, entryPoint.host);
-		await createDir(captureDir);
-		const screenShots = await ScreenShots.init(captureDir);
-		let i = 0;
-		for (const [url, page] of pageList) {
-			log(`Checking ${url} (${i} of ${pageList.size})`, 2);
-			const loaded = await scraper.checkPage(page);
-			if (loaded === true) {
-				page.screenshots = await screenShots.capture(url);
-			}
+    public async crawl() {
+        const {entryPoint, pageList, captureDir} = this;
 
-			this.writePage(page);
-			if (i++ % 50 === 0) {
-				this.writeList();
-			}
-		}
+        const scraper = new Scraper(pageList, entryPoint.host);
+        await createDir(captureDir);
+        const screenShots = await ScreenShots.init(captureDir);
+        let i = 0;
+        for (const [url, page] of pageList) {
+            log(`Checking ${url} (${i} of ${pageList.size})`, 2);
+            const loaded = await scraper.checkPage(page);
+            if (loaded === true) {
+                page.screenshots = await screenShots.capture(url);
+            }
 
-		await screenShots.close();
-		this.writeList();
-		bundle(this.captureDir);
-		return this.pageList;
-	}
+            this.writePage(page);
+            if (i++ % 50 === 0) {
+                this.writeList();
+            }
+        }
 
-	private writeList() {
-		fs.writeFileSync(Path.resolve(this.captureDir, 'crawl.json'), JSON.stringify(Array.from(this.pageList).map(a => a[0])));
-	}
+        await screenShots.close();
+        this.writeList();
+        bundle(this.captureDir);
+        return this.pageList;
+    }
 
-	private createSeed(): Page {
-		return {
-			url: this.entryPoint,
-			foundOn: 'Entry Point',
-		};
-	}
+    private writeList() {
+        fs.writeFileSync(Path.resolve(this.captureDir, 'crawl.json'), JSON.stringify(Array.from(this.pageList).map(a => a[0])));
+    }
 
-	private writePage(page: Page) {
-		fs.writeFileSync(Path.resolve(this.captureDir, sanitizeFileName(page.url.href)), JSON.stringify(page));
-	}
+    private createSeed(): Page {
+        return {
+            url: this.entryPoint,
+            foundOn: 'Entry Point',
+        };
+    }
+
+    private writePage(page: Page) {
+        fs.writeFileSync(Path.resolve(this.captureDir, sanitizeFileName(page.url.href)), JSON.stringify(page));
+    }
+
+    private mergeOptions(options: SnapshotOptions): ConcreteSnapshotOptions {
+        const defaultOnEvent = (msg: string) => console.log(msg);
+        return {
+            ignoreHead: options.ignoreHead ?? false,
+            screenshotsOnly: options.screenshotsOnly ?? false,
+            screenshotSizes: options.screenshotSizes ?? [],
+            htmlOnly: options.htmlOnly ?? false,
+            logLevel: options.logLevel ?? 2,
+            onEvent: options.onEvent ?? defaultOnEvent,
+        };
+    }
 }
--- src/types/options.d.ts
+++ src/types/options.d.ts
@@ -1,9 +1,15 @@
 export type SnapshotOptions = {
-	ignoreHead: boolean;
-	screenshotsOnly: boolean;
-	screenshotSizes: ScreenshotSize[];
-	htmlOnly: boolean;
+	ignoreHead?: boolean;
+	screenshotsOnly?: boolean;
+	screenshotSizes?: ScreenshotSize[];
+	htmlOnly?: boolean;
+    logLevel?: 1 | 2 | 3,
+    onEvent?: (msg: string) => void|Promise<void>
 };
+
+type ConcreteSnapshotOptions = {
+	[Property in keyof SnapshotOptions]-?: SnapshotOptions[Property];
+}
 
 export type ScreenshotSize = {
 	width: number;
