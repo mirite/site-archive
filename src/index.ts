03b6b6770f981d030bc8358125da3768fddf6334
Jesse Conner <jconner90@gmail.com>
2023-02-20 08:21:57 -0500

    Export all types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 import Crawler from './Crawler.js';
+export type {Page, PageList, SnapshotOptions, ScreenshotOptions, ScraperOptions, ConcreteOptions, ScreenshotSize} from 'types';
 export default Crawler;
